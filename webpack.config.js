1f6e9218de93e57d9e1dfe32a903225fcaf676d4
DzmitryLitvinau <angeridze93@gmail.com>
2019-11-24 18:53:45 +0300

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8080/`,
+    compress: true,
+    watchContentBase: true
+  }
+};
