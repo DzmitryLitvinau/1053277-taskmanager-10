96723a765f762beffd081d6d3a06ddda20c6e2fa
DzmitryLitvinau <angeridze93@gmail.com>
2019-12-02 17:52:00 +0300

    Implement logic of LoadMoreButton

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { generateTasks } from './mock/task.js';
 import { generateFilters } from './mock/filter.js';
 
 const TASK_COUNT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -25,7 +27,20 @@ const taskListElement = siteMainElement.querySelector(`.js-board__tasks`);
 
 const tasks = generateTasks(TASK_COUNT);
 renderTemplate(taskListElement, createEditTaskTemplate(tasks[0]), `beforeend`);
-tasks.slice(1).forEach((task) => renderTemplate(taskListElement, createTaskTemplate(task), `beforeend`));
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+tasks.slice(1, showingTasksCount).forEach((task) => renderTemplate(taskListElement, createTaskTemplate(task), `beforeend`));
 
 const boardElement = siteMainElement.querySelector(`.js-board`);
 renderTemplate(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+const loadMoreButton = boardElement.querySelector(`.load-more`);
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+  tasks.slice(prevTasksCount, showingTasksCount)
+    .forEach((task) => renderTemplate(taskListElement, createTaskTemplate(task), `beforeend`));
+
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});
