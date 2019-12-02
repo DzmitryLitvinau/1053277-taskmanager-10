00d14e7e2744cdcae6b5f5df3460f25d14920437
DzmitryLitvinau <angeridze93@gmail.com>
2019-12-02 15:33:10 +0300

    Change devtool option

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
-  devtool: `source-map`,
+  devtool: `eval-source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
     publicPath: `http://localhost:8080/`,
