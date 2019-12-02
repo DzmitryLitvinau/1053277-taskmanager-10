099456560e5c5012dfdef54857723d8c80ddc481
DzmitryLitvinau <angeridze93@gmail.com>
2019-12-02 17:36:14 +0300

    Add const.js

---
+++ src/const.js
@@ -0,0 +1,18 @@
+export const Colors = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+export const Days = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+export const MonthNames = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
